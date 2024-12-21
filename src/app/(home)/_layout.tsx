8ed86557afd3a08fda4e539bf2802f16864a461d
huyy2422 <huyyminer2411@gmail.com>
2024-12-21 20:48:16 +0700

    update _layout.tsx

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,5 +1,28 @@
 import { Slot, Stack } from "expo-router";
+import { useEffect } from "react";
+import { StreamChat } from "stream-chat";
+
+const client = StreamChat.getInstance("xg3agzbrbxgq");
 
 export default function HomeLayout() {
+    useEffect(() => {
+        const connect = async () => {
+            await client.connectUser(
+                {
+                  id: "ghuy",
+                  name: "Gia Huy",
+                  image: "https://i.imgur.com/fR9Jz14.png",
+                },
+                client.devToken("ghuy"),
+              );
+              const channel = client.channel("messaging", "the_halongans", {
+                name: "The Halongans",
+              });
+              await channel.watch();
+        };
+
+        connect();
+    })
+
     return <Slot/>;
 }

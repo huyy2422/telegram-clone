9aafdf498eb837acd2d6982acb96669fd2779cbc
huyy2422 <huyyminer2411@gmail.com>
2024-12-22 09:15:52 +0700

    da fix

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,36 +1,12 @@
-import { Slot, Stack } from "expo-router";
-import { useEffect } from "react";
-import { StreamChat } from "stream-chat";
-import { Chat, OverlayProvider} from 'stream-chat-expo';
-
-const client = StreamChat.getInstance("xg3agzbrbxgq");
+import { Stack } from "expo-router";
+import ChatProvider from "../../providers/ChatProvider";
 
 export default function HomeLayout() {
-    useEffect(() => {
-        const connect = async () => {
-            await client.connectUser(
-                {
-                  id: "ghuy",
-                  name: "Gia Huy",
-                  image: "https://i.imgur.com/fR9Jz14.png",
-                },
-                client.devToken("ghuy"),
-              );
-            //   const channel = client.channel("messaging", "the_halongans", {
-            //     name: "The Halongans",
-            //   });
-            //   await channel.watch();
-        };
-
-        connect();
-    })
-
-    return (
-        <OverlayProvider>
-        <Chat client={client}>
-        
-        <Stack><Stack.Screen name="(tabs)" options={{ headerShown: false }} /></Stack>
-        </Chat>
-      </OverlayProvider> 
-    );
+  return (
+    <ChatProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </ChatProvider>
+  );
 }
---
+++ src/providers/ChatProvider.tsx
@@ -0,0 +1,33 @@
+import { PropsWithChildren } from "react";
+import { useEffect } from "react";
+import { StreamChat } from "stream-chat";
+import { Chat, OverlayProvider } from "stream-chat-expo";
+
+const client = StreamChat.getInstance("xg3agzbrbxgq");
+
+export default function ChatProvider({ children }: PropsWithChildren) {
+  useEffect(() => {
+    const connect = async () => {
+      await client.connectUser(
+        {
+          id: "ghuy",
+          name: "Gia Huy",
+          image: "https://i.imgur.com/fR9Jz14.png",
+        },
+        client.devToken("ghuy")
+      );
+      //   const channel = client.channel("messaging", "the_halongans", {
+      //     name: "The Halongans",
+      //   });
+      //   await channel.watch();
+    };
+
+    connect();
+  });
+
+  return (
+    <OverlayProvider>
+      <Chat client={client}>{children}</Chat>
+    </OverlayProvider>
+  );
+}

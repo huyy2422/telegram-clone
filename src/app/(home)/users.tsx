b8594c88030dcd1d9d36822e8f908c6937223126
huyy2422 <huyyminer2411@gmail.com>
2024-12-26 19:45:45 +0700

    UserListItem setup

--- src/app/(home)/users.tsx
+++ src/app/(home)/users.tsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FlatList, Text } from "react-native";
+import { supabase } from "../../lib/supabase";
+import { useAuth } from "../../providers/AuthProvider";
+import UserListItem from "../../components/UserListItem";
 
 export default function UsersScreen() {
   const [users, setUsers] = useState([]);
+  const {user} = useAuth();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      let { data: profiles, error } = await supabase
+        .from("profiles")
+        .select("*")
+        .neq('id', user.id);
+      setUsers(profiles);
+    };
+    fetchUsers();
+  }, []);
 
   return (
     <FlatList
       data={users}
-      renderItem={({ item }) => <Text>{item.full_name}</Text>}
+      contentContainerStyle={{gap:5}}
+      renderItem={({ item }) => <UserListItem user={item}/>}
     />
   );
 }

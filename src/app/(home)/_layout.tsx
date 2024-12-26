4ed7875b6b8f3d5ab2d403b9797ec1ba1192bb5c
huyy2422 <huyyminer2411@gmail.com>
2024-12-26 11:58:41 +0700

    setting up profil tabs

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,5 +1,37 @@
 import { Slot, Stack } from "expo-router";
+import { useEffect } from "react";
+import { StreamChat } from "stream-chat";
+import { Chat, OverlayProvider } from "stream-chat-expo";
+
+const client = StreamChat.getInstance("7emjee66w6nw");
 
 export default function HomeLayout() {
-    return <Slot/>
+  useEffect(() => {
+    const connect = async () => {
+      await client.connectUser(
+        {
+          id: "ghuy",
+          name: "Gia Huy",
+          image: "https://i.imgur.com/fR9Jz14.png",
+        },
+        client.devToken("ghuy")
+      );
+
+      //   const channel = client.channel("messaging", "the_park", {
+      //     name: "The Park",
+      //   });
+
+      //   await channel.watch()
+    };
+
+    connect();
+  });
+
+  return (
+    <OverlayProvider>
+      <Chat client={client}>
+        <Slot />
+      </Chat>
+    </OverlayProvider>
+  );
 }

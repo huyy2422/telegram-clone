632d8ffe335cd7f9fbc94d75eabfd3872204cbf3
huyy2422 <huyyminer2411@gmail.com>
2024-12-26 18:11:53 +0700

    connect avt

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import { Channel, ChannelList, Message, MessageInput, MessageList } from 'stream-chat-expo';
 import { Channel as ChannelType, StreamChat } from 'stream-chat';
 import { router } from 'expo-router';
+import { useAuth } from '../../../providers/AuthProvider';
 
 export default function MainTabScreen() {
-    return <ChannelList onSelect={(channel) => router.push(`/channel/${channel.cid}`)}/> 
+    const { user } = useAuth()
+    return <ChannelList filters={{members: {$in: [user.id]}}} onSelect={(channel) => router.push(`/channel/${channel.cid}`)}/> 
 }
